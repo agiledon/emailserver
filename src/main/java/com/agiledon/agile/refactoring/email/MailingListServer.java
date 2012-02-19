3f6ec03cb00bf01cf6a91a8ff3a3004a64c3dff3
Yi Zhang <bruce.e.zhang@gmail.com>
2012-02-19 17:55:12 +0800

    extract receiveMessages() and sendMessage() methods

--- src/main/java/com/agiledon/agile/refactoring/email/MailingListServer.java
+++ src/main/java/com/agiledon/agile/refactoring/email/MailingListServer.java
@@ -1,5 +1,6 @@
 package com.agiledon.agile.refactoring.email;
 
+import java.io.IOException;
 import java.util.Properties;
 
 import javax.mail.*;
@@ -27,6 +28,7 @@ public class MailingListServer {
         try {
             do {
                 try {
+
                     Properties properties = System.getProperties();
                     Session session = Session.getDefaultInstance(properties, null);
                     Store store = session.getStore("pop3");
@@ -36,57 +38,32 @@ public class MailingListServer {
                         return;
                     }
                     Folder folder = defaultFolder.getFolder("INBOX");
+
                     if (folder == null) {
                         return;
                     }
                     folder.open(FOLDER.READ_WRITE);
                     try {
                         if (folder.getMessageCount() != 0) {
-                            Message[] messages = folder.getMessages();
-                            FetchProfile fp = new FetchProfile();
-                            fp.add(FetchProfile.Item.ENVELOPE);
-                            fp.add(FetchProfile.Item.FLAGS);
-                            fp.add("X-Mailer");
-                            folder.fetch(messages, fp);
+                            Message[] messages = null;
+
+                            messages = receiveMessages(folder);
+
                             for (int i = 0; i < messages.length; i++) {
-                                Message message = messages[i];
-                                if (message.getFlags().contains(Flags.Flag.DELETED)) continue;
-                                System.out.println("message received: " + message.getSubject());
-                                if (!roster.constainsOneOf(message.getFrom())) continue;
-                                MimeMessage forward = new MimeMessage(session);
-                                Address[] fromAddress = message.getFrom();
-                                InternetAddress from = null;
-                                if (fromAddress != null && fromAddress.length > 0) {
-                                    from = new InternetAddress(fromAddress[0].toString());
-                                }
-                                forward.setFrom(from);
-                                forward.setReplyTo(new Address[]{
-                                        new InternetAddress(listAddress)
-                                });
-
-                                forward.addRecipients(Message.RecipientType.BCC, roster.getAddresses());
-                                String subject = message.getSubject();
-                                if (-1 == message.getSubject().indexOf(SUBJECT_MARKER)) {
-                                    subject = SUBJECT_MARKER + " " + message.getSubject();
-                                }
-                                forward.setSubject(subject);
-                                forward.setSentDate(message.getSentDate());
-                                forward.addHeader(LOOP_HEADER, listAddress);
-                                Object content = message.getContent();
-                                if (content instanceof Multipart) {
-                                    forward.setContent((Multipart) content);
-                                } else {
-                                    forward.setText((String) content);
-                                }
-
                                 Properties props = new Properties();
-                                props.put("mail.smtp.host", host.smtpHost);
-
                                 Session smtpSession = Session.getDefaultInstance(props, null);
                                 Transport transport = smtpSession.getTransport("smtp");
-                                transport.connect(host.smtpHost, host.smtpUser, host.smtpPassword);
-                                transport.sendMessage(forward, roster.getAddresses());
-                                message.setFlag(Flags.Flag.DELETED, true);
+
+
+                                Message message = messages[i];
+
+                                if (isDeletedMessage(message)) continue;
+
+                                System.out.println("message received: " + message.getSubject());
+
+                                if (!roster.constainsOneOf(message.getFrom())) continue;
+
+                                sendMessage(host, listAddress, roster, session, props, transport, message);
                             }
                         }
                     } catch (Exception e) {
@@ -110,6 +87,56 @@ public class MailingListServer {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static Message[] receiveMessages(Folder folder) throws MessagingException {
+        Message[] messages;
+        messages = folder.getMessages();
+        FetchProfile fp = new FetchProfile();
+        fp.add(FetchProfile.Item.ENVELOPE);
+        fp.add(FetchProfile.Item.FLAGS);
+        fp.add("X-Mailer");
+        folder.fetch(messages, fp);
+        return messages;
+    }
+
+    private static void sendMessage(HostInformation host, String listAddress, Roster roster, Session session, Properties props, Transport transport, Message message) throws MessagingException, IOException {
+        MimeMessage forward = new MimeMessage(session);
+        Address[] fromAddress = message.getFrom();
+        InternetAddress from = null;
+        if (fromAddress != null && fromAddress.length > 0) {
+            from = new InternetAddress(fromAddress[0].toString());
+        }
+        forward.setFrom(from);
+        forward.setReplyTo(new Address[]{
+                new InternetAddress(listAddress)
+        });
+
+        forward.addRecipients(Message.RecipientType.BCC, roster.getAddresses());
+        String subject = message.getSubject();
+        if (-1 == message.getSubject().indexOf(SUBJECT_MARKER)) {
+            subject = SUBJECT_MARKER + " " + message.getSubject();
+        }
+        forward.setSubject(subject);
+        forward.setSentDate(message.getSentDate());
+        forward.addHeader(LOOP_HEADER, listAddress);
+        Object content = message.getContent();
+        if (content instanceof Multipart) {
+            forward.setContent((Multipart) content);
+        } else {
+            forward.setText((String) content);
+        }
+
+        props.put("mail.smtp.host", host.smtpHost);
+
+
+        transport.connect(host.smtpHost, host.smtpUser, host.smtpPassword);
+        transport.sendMessage(forward, roster.getAddresses());
+        message.setFlag(Flags.Flag.DELETED, true);
+    }
+
+    private static boolean isDeletedMessage(Message message) throws MessagingException {
+        return message.getFlags().contains(Flags.Flag.DELETED);
     }
 
 }
