788322bdc40a1e16735e0d2abe7adc3e11ddf618
Yi Zhang <bruce.e.zhang@gmail.com>
2012-02-22 12:43:55 +0800

    refactoring

--- src/main/java/com/agiledon/agile/refactoring/email/MailingListServer.java
+++ src/main/java/com/agiledon/agile/refactoring/email/MailingListServer.java
@@ -1,6 +1,5 @@
 package com.agiledon.agile.refactoring.email;
 
-import java.io.IOException;
 import java.util.Properties;
 
 import javax.mail.*;
@@ -33,10 +32,15 @@ public class MailingListServer {
         try {
             do {
                 try {
+<<<<<<< HEAD
 
                     properties = System.getProperties();
                     session = Session.getDefaultInstance(properties, null);
 
+=======
+                    Properties properties = System.getProperties();
+                    Session session = Session.getDefaultInstance(properties, null);
+>>>>>>> parent of 3f6ec03... extract receiveMessages() and sendMessage() methods
                     Store store = session.getStore("pop3");
                     store.connect(host.pop3Host, -1, host.pop3User, host.pop3Password);
                     Folder defaultFolder = store.getDefaultFolder();
@@ -44,17 +48,65 @@ public class MailingListServer {
                         return;
                     }
                     Folder folder = defaultFolder.getFolder("INBOX");
-
                     if (folder == null) {
                         return;
                     }
                     folder.open(FOLDER.READ_WRITE);
                     try {
                         if (folder.getMessageCount() != 0) {
+<<<<<<< HEAD
                             Message[] messages = null;
 
                             messages = receiveMessages(folder);
                             sendMessages(messages);
+=======
+                            Message[] messages = folder.getMessages();
+                            FetchProfile fp = new FetchProfile();
+                            fp.add(FetchProfile.Item.ENVELOPE);
+                            fp.add(FetchProfile.Item.FLAGS);
+                            fp.add("X-Mailer");
+                            folder.fetch(messages, fp);
+                            for (int i = 0; i < messages.length; i++) {
+                                Message message = messages[i];
+                                if (message.getFlags().contains(Flags.Flag.DELETED)) continue;
+                                System.out.println("message received: " + message.getSubject());
+                                if (!roster.constainsOneOf(message.getFrom())) continue;
+                                MimeMessage forward = new MimeMessage(session);
+                                Address[] fromAddress = message.getFrom();
+                                InternetAddress from = null;
+                                if (fromAddress != null && fromAddress.length > 0) {
+                                    from = new InternetAddress(fromAddress[0].toString());
+                                }
+                                forward.setFrom(from);
+                                forward.setReplyTo(new Address[]{
+                                        new InternetAddress(listAddress)
+                                });
+
+                                forward.addRecipients(Message.RecipientType.BCC, roster.getAddresses());
+                                String subject = message.getSubject();
+                                if (-1 == message.getSubject().indexOf(SUBJECT_MARKER)) {
+                                    subject = SUBJECT_MARKER + " " + message.getSubject();
+                                }
+                                forward.setSubject(subject);
+                                forward.setSentDate(message.getSentDate());
+                                forward.addHeader(LOOP_HEADER, listAddress);
+                                Object content = message.getContent();
+                                if (content instanceof Multipart) {
+                                    forward.setContent((Multipart) content);
+                                } else {
+                                    forward.setText((String) content);
+                                }
+
+                                Properties props = new Properties();
+                                props.put("mail.smtp.host", host.smtpHost);
+
+                                Session smtpSession = Session.getDefaultInstance(props, null);
+                                Transport transport = smtpSession.getTransport("smtp");
+                                transport.connect(host.smtpHost, host.smtpUser, host.smtpPassword);
+                                transport.sendMessage(forward, roster.getAddresses());
+                                message.setFlag(Flags.Flag.DELETED, true);
+                            }
+>>>>>>> parent of 3f6ec03... extract receiveMessages() and sendMessage() methods
                         }
                     } catch (Exception e) {
                         System.err.println("message handling error");
@@ -79,6 +131,7 @@ public class MailingListServer {
         }
     }
 
+<<<<<<< HEAD
     private static void sendMessages(Message[] messages) throws MessagingException, IOException {
         for (int i = 0; i < messages.length; i++) {
             Message message = messages[i];
@@ -144,4 +197,6 @@ public class MailingListServer {
         return message.getFlags().contains(Flags.Flag.DELETED);
     }
 
+=======
+>>>>>>> parent of 3f6ec03... extract receiveMessages() and sendMessage() methods
 }
